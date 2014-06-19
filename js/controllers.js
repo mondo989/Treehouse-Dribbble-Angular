40d391c7aab0f38ec18590db015cb781b31d5a48
Armando <flores@mtu.edu>
2014-06-19 01:06:37 -0700

    It finally worked!

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,15 @@
 
 var controllers = angular.module('dabbble.controllers', []);
 
-controllers.controller('AppCtrl', funtion ($scope) {
-   $scope.name = "Module";
+controllers.controller('AppCtrl', function ($scope) {
+      $scope.name = "Module";
+});
+
+controllers.controller('ShotsListCtrl', function ($scope, $http) {
+      $scope.list;
+
+      $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function (data) {
+            $scope.list = data.data;
+            console.log(data);
+      })
 })
